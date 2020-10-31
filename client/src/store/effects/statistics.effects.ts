e1c0d4c0aff5763f4aa9fc739e650c6764d7ec9b
igalM <igalmm@gmail.com>
2020-10-31 21:48:25 +0200

    update effects

--- client/src/store/effects/statistics.effects.ts
+++ client/src/store/effects/statistics.effects.ts
@@ -1,11 +1,11 @@
 import { Injectable } from "@angular/core";
 import { createEffect, Actions, ofType } from '@ngrx/effects';
-import { switchMap, concatMap, map, mergeMap } from 'rxjs/operators';
+import { switchMap, concatMap } from 'rxjs/operators';
 import { StatisticsService } from 'src/app/shared/services/statistics.service';
 import * as StatisticsActions from '../actions/statistics.actions';
 import { TotalStatisticDto } from 'src/app/shared/models/total.statistic.dto';
 import { DailyStatisticDto } from 'src/app/shared/models/daily.statistic.dto';
-import { forkJoin } from 'rxjs';
+import { combineLatest } from 'rxjs';
 
 @Injectable()
 export class StatisticsEffects {
@@ -19,17 +19,14 @@ export class StatisticsEffects {
         switchMap(() => {
             const total$ = this.statisticsService.getTotal();
             const daily$ = this.statisticsService.getDaily();
-            const totalDaily = this.statisticsService.getTotalDaily();
-            return forkJoin([total$, daily$, totalDaily]).pipe(
-                concatMap(([a, b, c]) => [
-                    StatisticsActions.fetchTotalSuccess({ statistics: a }),
-                    StatisticsActions.fetchDailySuccess({ statistics: b }),
-                    StatisticsActions.fetchTotalDailySuccess({ statistics: c })
-                ])
-            )
-        })
-    )
-    )
+            const totalDaily$ = this.statisticsService.getTotalDaily();
+            return combineLatest([total$, daily$, totalDaily$]);
+        }),
+        concatMap(([total, daily, totalDaily]) => [
+            StatisticsActions.fetchTotalSuccess({ statistics: total }),
+            StatisticsActions.fetchDailySuccess({ statistics: daily }),
+            StatisticsActions.fetchTotalDailySuccess({ statistics: totalDaily })
+        ])));
 
     constructor(
         private readonly actions$: Actions,
